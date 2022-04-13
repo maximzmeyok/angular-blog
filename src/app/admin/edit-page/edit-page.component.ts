347f5b060b75a94603e2eb5976e8a07da4daf68c
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-04-13 10:09:42 +0300

    feat: made a project

---
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription, switchMap } from 'rxjs';
+import { PostsService } from 'src/app/shared/posts.service';
+import { Post } from '../../shared/interfaces';
+import { AlertService } from '../shared/services/alert.service';
+
+@Component({
+  selector: 'app-edit-page',
+  templateUrl: './edit-page.component.html',
+  styleUrls: ['./edit-page.component.scss']
+})
+export class EditPageComponent implements OnInit, OnDestroy {
+
+  form: FormGroup
+  post: Post
+  submitted = false
+
+  uSub: Subscription
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private alert: AlertService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+      })
+    ).subscribe((post: Post) => {
+      this.post = post
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required)
+      })
+    })
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    this.submitted = true
+
+    this.uSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title
+    }).subscribe(() => {
+      this.submitted = false
+      this.alert.warning('Post have been refreshed')
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.uSub) {
+      this.uSub.unsubscribe()
+    }
+  }
+}
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostsService } from '../shared/posts.service';
+import { Post } from '../shared/interfaces';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  posts$: Observable<Post[]>
+
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.posts$ = this.postsService.getAll()
+  }
+
+}
